ab854471f078580a6c508ae1cec40fb35cf12d9a
Diane DELORD <82445395+DianeDelord@users.noreply.github.com>
2022-01-04 18:27:10 +0100

    message erreur si extension du justif incorrecte

--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -2,7 +2,7 @@ import VerticalLayout from './VerticalLayout.js'
 
 export default () => {
 
-  return (`
+    return (`
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -56,6 +56,7 @@ export default () => {
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
                     <input required type="file" class="form-control blue-border" data-testid="file" />
+                    <p class="error-file-type hidden">Seuls les fichiers .jpg, .jpeg et .png sont acceptés.</p>
                   </div>
                 </div>
             </div>
