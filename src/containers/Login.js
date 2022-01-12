2e32517e2e7639b8e1bc5288c040ce00f7cc0904
Diane DELORD <82445395+DianeDelord@users.noreply.github.com>
2022-01-12 14:09:27 +0100

    update istanbul ignore test

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -58,6 +58,7 @@ export default class Login {
     }
 
     // not need to cover this function by tests
+    /* istanbul ignore next*/
     login = (user) => {
         if (this.store) {
             return this.store
@@ -73,6 +74,7 @@ export default class Login {
     }
 
     // not need to cover this function by tests
+    /* istanbul ignore next*/
     createUser = (user) => {
         if (this.store) {
             return this.store
